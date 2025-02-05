b3cf3dd77888cafbeb81e914523c2f219987f9a0
SHIVAM RAWAT <shivam635933@gmail.com>
2025-02-06 00:23:46 +0530

    Files for the Aqua Fish  in the debug  Problem

---
+++ Web Technology/Debug - JavaScript/Aqua Fish Stall/script.js
@@ -0,0 +1,54 @@
+function getDisplay() 
+{ 
+    try {
+        var name = document.getElementById("name").value; 
+        var phonenumber = document.getElementById("phonenumber").value; 
+        var total = getTotalAmount(); 
+        document.getElementById("result").innerHTML="Please pay $"+total; 
+    } catch(err) { 
+        document.getElementById("result").innerHTML=err; 
+    } 
+    return false; 
+} 
+
+function getTotalAmount() 
+{  
+    var sum = 0; 
+    try 
+    { 
+        if(document.getElementById("anchovy").value) { 
+            sum += document.getElementById("anchovy").value * 2; 
+        } 
+        if(document.getElementById("barracuda").value) { 
+            sum += document.getElementById("barracuda").value * 5; 
+        } 
+        if(document.getElementById("crab").value) { 
+            sum += document.getElementById("crab").value * 3; 
+        }
+        if(document.getElementById("kingmackerel").value) { 
+            sum += document.getElementById("kingmackerel").value * 10; 
+        } 
+        if(document.getElementById("pomfret").value) { 
+            sum += document.getElementById("pomfret").value * 9; 
+        } 
+        if(document.getElementById("prawn").value) { 
+            sum += document.getElementById("prawn").value * 3;  
+        } 
+        if(document.getElementById("salmon").value) { 
+            sum += document.getElementById("salmon").value * 6; 
+        } 
+        if(document.getElementById("sardine").value){ 
+            sum += document.getElementById("sardine").value * 2; 
+        } 
+        if(document.getElementById("shark").value) { 
+            sum += document.getElementById("shark").value * 5; 
+        } 
+        if(document.getElementById("tilapia").value) { 
+            sum += document.getElementById("tilapia").value * 4; 
+        } 
+    
+        return sum; 
+    } catch(err) { 
+        document.getElementById("result").innerHTML=err; 
+    } 
+}
