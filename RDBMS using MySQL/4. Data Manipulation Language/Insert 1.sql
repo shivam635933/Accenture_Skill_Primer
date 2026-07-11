d8d3525236caf514a45375cc0feb1c3948354117
SACHIN VARDHAN <165420880+Sachin9vardhan@users.noreply.github.com>
2026-07-12 00:47:13 +0530

    Consolidate insert statements into one
    Replaced multiple insert statements with a single insert statement for better readability and efficiency.

--- RDBMS using MySQL/4. Data Manipulation Language/Insert 1.sql
+++ RDBMS using MySQL/4. Data Manipulation Language/Insert 1.sql
@@ -1,8 +1,3 @@
-insert into Booking_Master Values ('B101', 'ΕΝ1001', 40000, 'Cash'); 
-insert into Booking_Master Values ('B102', 'EN1012', 750000, 'Online'); 
-insert into Booking_Master Values('B103', 'EN1016', 16000, 'Cheque'); 
-insert into Booking_Master Values ('B104', 'EN1007', 10000, 'Cash');
-
 
 INSERT INTO Booking_Master (Booking_id, Enquiry_Id, Total_amount, Mode_of_Pay) 
 VALUES 
@@ -10,5 +5,3 @@ VALUES
 ('B102', 'EN1012', 750000, 'Online'),
 ('B103', 'EN1016', 16000, 'Cheque'),
 ('B104', 'EN1007', 10000, 'Cash');
-
-SELECT * FROM Booking_Master;
